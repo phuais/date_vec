47989e315dfd7dbae4a17abbab92e44b43bb67a8
Pablo Yair Huais <phuais@gmail.com>
2020-03-20 23:56:33 -0300

    Update date_vec.R

--- date_vec.R
+++ date_vec.R
@@ -1,6 +1,6 @@
 # Input numeric values for day, month, year and total days.
 # Customize the format of the date. Choose between "d", "m", "y", "dm", "mdy" or "dmy"
-# Choose the character string to separate the terms (only valid when format is "dm", "mdy" or "dmy")
+# Choose the character string that will separate the terms (only valid when format is "dm", "mdy" or "dmy")
 #
 # The function outputs a character vector of length defined in total_days, each element representing
 # a date, starting from the day, month and year chosen by the user, with a particular format also 
